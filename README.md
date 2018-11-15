94ec6177a0e1feeff1f522894f2791c5288c0c26
jialin <alonesjialin@gmail.com>
2018-11-15 18:58:38 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # react-dynamic-antd-theme
-configure antd theme dynamically in create-react-app
+
+configure antd theme dynamically in create-react-app 2.0 use [customize-cra](https://github.com/arackaf/customize-cra)
+
+fork from [react-app-rewire-antd-theme](https://github.com/mzohaibqc/react-app-rewire-antd-theme)
+
+#install
+
+```
+npm install --save-dev react-dynamic-antd-theme
+```
+
+#usage
+
+just add a file `config-overrides.js` in root directory
+and add following code in it. You either need to provide `variables` less file or variables object.
+
+```js
+const path = require("path");
+const customizeCra = require("customize-cra");
+const dynamicAntdTheme = require("react-dynamic-antd-theme");
+const { override, useEslintRc, useBabelRc } = customizeCra;
+
+const options = {
+  stylesDir: path.join(__dirname, "./src/styles"),
+  antDir: path.join(__dirname, "./node_modules/antd"),
+  varFile: path.join(__dirname, "./src/styles/variables.less"),
+  mainLessFile: path.join(__dirname, "./src/styles/index.less"),
+  themeVariables: ["@primary-color"],
+  indexFileName: "index.html",
+  generateOnce: false,
+  publicPath: "" // e.g. in case you are hosting at gh-pages `https://username.github.io/project` then you can add `/project` here
+};
+module.exports = override(
+  useEslintRc(),
+  useBabelRc(),
+  dynamicAntdTheme(options)
+);
+```
+
+Default paths for various files are as in above snippet but you can override by passing your own values.
+`themeVariables` is required field (if you want to generate color.less file for Dynamic theme) and it's an array of color variable names that you want to configure for Dynamic theme e.g. ['@primary-color', '@secondry-color']
+Here are two color specified in array. First one is Ant Design specific and other is our custom one. You can use Ant Design color variables as well as your own custom variables as in above example.
+
+## Note: generateOnce: false
+
+By default this option is set as `false` which means that on each compilation, `color.less` file will be generated for dynamic theming but if you want you make your build/compilation process fast, you can disable `color.less` file generation on each compilation by setting this parameter to `true`and it will only be generated on first compilation/build. But if you have changes in your styles/less then you need to restart your compilation process `npm start` to generate `color.less` with updated changes.
+
+# Utilities
+
+- getLessVars(filePath)
+
+This function reads a less file and create an object with keys as variable names
+and values as variables respective values. e.g. variabables.less
+
+```
+@primary-color : #1890ff;
+@heading-color : #fa8c16;
+@text-color : #cccccc;
+```
+
+to
+
+```js
+{
+  '@primary-color' : '#1890ff',
+  '@heading-color' : '#fa8c16',
+  '@text-color' : '#cccccc'
+}
+```
+
+- isValidColor(color)
+
+This method takes color string as input and return true if string is a valid color otherwise returns false with one exception that if a color matches this regex `/colorPalette|fade/g` it will return true e.g.
+
+```
+isValidColor('#ffffff'); //true
+isValidColor('#fff'); //true
+isValidColor('rgba(0, 0, 0, 0.5)'); //true
+isValidColor('20px'); //false
+```
---
+++ index.js
@@ -0,0 +1,113 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+const postcss = require("postcss");
+const less = require("less");
+const bundle = require("less-bundle-promise");
+const { injectBabelPlugin } = require("react-app-rewired");
+const rewireLess = require("react-app-rewire-less");
+const AntDesignThemePlugin = require("./webpack-plugins");
+
+function updateConfig(config, env, options = {}) {
+  let vars = {};
+  if (options.varFile) {
+    if (fs.existsSync(options.varFile)) {
+      vars = getLessVars(options.varFile);
+    } else {
+      throw Error("variables file absolute path does not exists. ");
+    }
+  } else {
+    const stylesDir =
+      options.stylesDir || path.join(__dirname, "../../src/styles");
+    const varFile = path.join(stylesDir, "./variables.less");
+    if (fs.existsSync(varFile)) {
+      vars = getLessVars(varFile);
+    } else {
+      console.log(`No variables.less file found at path: ${varFile}`);
+    }
+  }
+  /*
+    One can provide variables through options against `vars` key or it can be used to override
+    variables' values provides in variables file
+  */
+  vars = Object.assign(vars, options.vars);
+  config = rewireLess.withLoaderOptions({
+    modifyVars: vars,
+    javascriptEnabled: true
+  })(config, env);
+  if (!(options.importPlugin === false)) {
+    config = injectBabelPlugin(
+      ["import", { libraryName: "antd", style: true }],
+      config
+    );
+  }
+  if (
+    Array.isArray(options.themeVariables) &&
+    options.themeVariables.length > 0
+  ) {
+    config.plugins.push(new AntDesignThemePlugin(options));
+  }
+
+  return config;
+}
+
+/*
+  This funtion reads a less file and create an object with keys as variable names 
+  and values as variables respective values. e.g.
+  //variabables.less
+    @primary-color : #1890ff;
+    @heading-color : #fa8c16;
+    @text-color : #cccccc;
+  
+    to
+
+    {
+      '@primary-color' : '#1890ff',
+      '@heading-color' : '#fa8c16',
+      '@text-color' : '#cccccc'
+    }
+
+*/
+function getLessVars(filtPath) {
+  const sheet = fs.readFileSync(filtPath).toString();
+  const lessVars = {};
+  const matches = sheet.match(/@(.*:[^;]*)/g) || [];
+
+  matches.forEach(variable => {
+    const definition = variable.split(/:\s*/);
+    const varName = definition[0].replace(/['"]+/g, "").trim();
+    lessVars[varName] = definition.splice(1).join(":");
+  });
+  return lessVars;
+}
+
+/*
+  This function takes color string as input and return true if string is a valid color otherwise returns false.
+  e.g.
+  isValidColor('#ffffff'); //true
+  isValidColor('#fff'); //true 
+  isValidColor('rgba(0, 0, 0, 0.5)'); //true
+  isValidColor('20px'); //false
+*/
+function isValidColor(color) {
+  if (!color || color.match(/px/g)) return false;
+  if (color.match(/colorPalette|fade/g)) return true;
+  if (color.charAt(0) === "#") {
+    color = color.substring(1);
+    return (
+      [3, 4, 6, 8].indexOf(color.length) > -1 && !isNaN(parseInt(color, 16))
+    );
+  }
+  return /^(rgb|hsl)a?\((\d+%?(deg|rad|grad|turn)?[,\s]+){2,3}[\s\/]*[\d\.]+%?\)$/i.test(
+    color
+  );
+}
+
+function dynamicAntdTheme(options) {
+  return function(config) {
+    return updateConfig(config, process.env.NODE_ENV, options);
+  };
+}
+
+module.exports = dynamicAntdTheme;
